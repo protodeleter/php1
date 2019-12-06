9e433fa040bbcf431d32a4824b2b161a5fbc7a86
protodeleter <pavel.nsnv@gmail.com>
2019-12-06 14:16:50 +0200

    ...

---
+++ OOP/GuestBook.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulPc
+ * Date: 06.12.2019
+ * Time: 11:49
+ */
+
+class GuestBook
+{
+
+}
---
+++ OOP/guest_records.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulPc
+ * Date: 06.12.2019
+ * Time: 11:51
+ */
