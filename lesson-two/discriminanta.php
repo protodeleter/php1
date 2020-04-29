14571c6a6ccfabb7a949ac88c9ba2589e03bb5ff
protodeleter <pavel.nsnv@gmail.com>
2020-04-30 00:51:08 +0300

    ...

--- lesson-two/discriminanta.php
+++ lesson-two/discriminanta.php
@@ -7,22 +7,19 @@
  */
 zend.assertions == 1;
 
-function discriminanta( $a = false ,$b = false ,$c = false ) {
 
-    // $a,$b,$c,$x
 
-    $a;
-    $b;
-    $c;
-    $x;
-    $d; // discriminanta
+function discriminanta2( $a = false ,$b = false ,$c = false ) {
 
-    $x1;
-    $x2;
+    assert( !is_int ($a) , '$a is not 1');
 
+
+    $d = pow($b,2) - ( 4*$a*$c );
     if ( !$c ) {
         $c == 0;
     }
+
+
     if ( !$b ) {
         $a == 0;
     }
@@ -32,51 +29,13 @@ function discriminanta( $a = false ,$b = false ,$c = false ) {
     if ( $a == 0 ) {
         return 'коэффициент "a" не может равняться нулю';
     }
-
-    // formula
-    // ax2 + bx + c = 0
-    // $a * pow($x , 2) + $b*$x + $c = 0;
-    //D = b2 − 4ac.
-
-    $d = pow($b,2) - ( 4*$a*$c );
-
-    if ($d > 0) {
-     $x1 = ( (-$b) + sqrt($d) ) / 2*$a;
-     $x2 = ( (-$b) - sqrt($d) ) / 2*$a;
-     $x = 'вводные данные ' . $a . ' ' . $b . ' ' . $c . ' <br/> ' .'первый корень '. $x1 .'<br/> второй корень '. $x2. ' <br /> ' . 'дискриминант ' . $d . '<br/>';
-
-    } else if ( $d == 0 ) {
-
-//        echo $d . ' 2';
-
-     $x = ( -$b ) / (2*$a);
-     return 'вводные данные ' . $a . ' ' . $b . ' ' . $c . ' <br/> ' . 'единственный корень '.$x . '<br/> дискриминант ' . $d . '<br/>';
-
-    } else {
-
-//    echo $d . ' 3';
-
-     return 'вводные данные ' . $a . ' ' . $b . ' ' . $c . ' <br/> ' . 'дискриминант ' . $d  .'<br/>'. ' корней нет' . '<br/>';
-
-    }
-
- return $x;
-
+    return $d;
 }
 
 
-echo discriminanta(1,-1,-90 );
-
-echo '<br/>';
-echo '<br/>';
 
 
-echo discriminanta(1,12,36 );
 
-echo '<br/>';
-echo '<br/>';
-
-echo discriminanta(3,4,10 );
-
+echo 'дискриминанта = '. discriminanta2(1,e,-90 );
 
 
